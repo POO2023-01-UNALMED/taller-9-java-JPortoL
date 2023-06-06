5430fabef8db5f8c69c0dd073f29ee23c0caf2e8
JPortoL <jporto@unal.edu.co>
2023-06-05 23:04:15 -0500

    Update Calculadora.java

--- src/main/java/gui/Calculadora.java
+++ src/main/java/gui/Calculadora.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import gui.Calculator;
 
 public class Calculadora extends Application  {
 	
